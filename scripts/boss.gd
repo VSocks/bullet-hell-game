a8aaafb2f3459e904c74852e222ad06e2ea31858
VSocks <marvinpsilva2003@gmail.com>
2025-08-18 17:36:58 -0300

    adicionados novos scripts de padrões de ataque para o chefão. Por hora ele apenas usa o mesmo ataque com balas diferentes dependendo da quantidade de vida, mas no futuro farei alterações mais significativas e adicionarei movimento.

--- scripts/boss.gd
+++ scripts/boss.gd
@@ -2,9 +2,9 @@ extends CharacterBody2D
 class_name Boss
 
 signal shoot1
-#signal shoot2
+signal shoot2
+signal shoot3
 #signal shoot4
-#signal shoot5
 
 var max_health : int = 1000
 var health : int
@@ -16,13 +16,12 @@ func _ready():
 func _process(delta):
 	if health >= 0.75 * max_health:
 		shoot1.emit()
-	#possíveis outras fases do boss
-	#if 0.75 * max_health > health && health >= 0.5 * max_health:
-	#	shoot2.emit()
-	#if 0.5 * max_health > health && health >= 0.25 * max_health:
-	#	shoot4.emit()
+	if 0.75 * max_health > health && health >= 0.5 * max_health:
+		shoot2.emit()
+	if 0.5 * max_health > health && health >= 0.25 * max_health:
+		shoot3.emit()
 	#if 0.25 * max_health > health:
-	#	shoot5.emit()
+	#	shoot4.emit()
 
 func take_damage(damage):
 	print("boss takes damage!")
--- scripts/boss_attack_pattern_3.gd
+++ scripts/boss_attack_pattern_3.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var bullet_scene_round : PackedScene = preload("res://scenes/enemy_bullet_round.tscn")
+var bullet_scene_laser : PackedScene = preload("res://scenes/enemy_bullet_laser.tscn")
 
 var angle : float = 0.0
 var spiral_speed : float = 5.0
@@ -8,9 +8,9 @@ var bullet_count : int = 4
 var bullet
 
 
-func _on_boss_shoot_1():
+func _on_boss_shoot_3():
 	for i in range(bullet_count):
-		bullet = bullet_scene_round.instantiate()
+		bullet = bullet_scene_laser.instantiate()
 		get_tree().current_scene.add_child(bullet)
 		var direction = Vector2(cos(angle), sin(angle))
 		bullet.global_position = global_position
