4fd4c765b265375a1c66eedfa143edc78ef5f075
VSocks <marvinpsilva2003@gmail.com>
2025-08-22 20:35:51 -0300

    modificado tamanhos dos nodes visibleonscreennotifier2d das bullets inimigas

--- scripts/bullet_pool.gd
+++ scripts/bullet_pool.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var bullet_scene : PackedScene = preload("res://scenes/enemy_bullet_missile.tscn")
+var bullet_scene : PackedScene = preload("res://scenes/enemy_bullet_square.tscn")
 var pool_size : int = 0
 var available_bullets : Array = []
 var expansion : int = 0
@@ -13,7 +13,7 @@ func _ready():
 		bullet.process_mode = Node.PROCESS_MODE_DISABLED
 		add_child(bullet)
 		available_bullets.append(bullet)
-	print("Bullet pool initialized with ", pool_size, " bullets")	
+	#print("Bullet pool initialized with ", pool_size, " bullets")	
 
 
 func return_bullet(bullet: Area2D) -> void:
@@ -21,7 +21,7 @@ func return_bullet(bullet: Area2D) -> void:
 	bullet.process_mode = Node.PROCESS_MODE_DISABLED
 	bullet.position = Vector2(-1000, -1000)  # Move off-screen
 	available_bullets.append(bullet)
-	print("Bullet returned to pool!")
+	#print("Bullet returned to pool!")
 
 
 func get_bullet() -> Area2D:
@@ -30,10 +30,10 @@ func get_bullet() -> Area2D:
 		bullet = bullet_scene.instantiate()
 		add_child(bullet)
 		expansion += 1
-		print("New bullet created, bullet pool expanded to size ", pool_size + expansion)
+		#print("New bullet created, bullet pool expanded to size ", pool_size + expansion)
 	else:
 		bullet = available_bullets.pop_back()
-		print("Bullet pulled from pool!")
+		#print("Bullet pulled from pool!")
 	bullet.visible = true
 	bullet.process_mode = Node.PROCESS_MODE_INHERIT
 	bullet.play_sound()
