955c135baacf739ca499848da9134ff1b0b18cca
VSocks <marvinpsilva2003@gmail.com>
2025-08-28 22:49:25 -0300

    diminuidas hitboxes de algumas balas por motivos de balanceamento. também modificado ataque do chefão.

--- scripts/boss_attack_spiral.gd
+++ scripts/boss_attack_spiral.gd
@@ -1,15 +1,15 @@
 extends Node2D
 
-const BULLET_COUNT : int = 36
-const MAX_BURST_SHOTS : int = 12
-const SHOT_INTERVAL : float = 0.075
+const BULLET_COUNT : int = 30
+const MAX_BURST_SHOTS : int = 16
+const SHOT_INTERVAL : float = 0.125
 const COOLDOWN : float = 1.5
 const TOTAL_CYCLE_TIME : float = (MAX_BURST_SHOTS * SHOT_INTERVAL) + COOLDOWN + 0.1
-const ANGLE_INCREMENT : float = 30
+const ANGLE_INCREMENT : float = 5
 
 var burst_count : int = 0
 var bullet_index : int = 0
-var bullet_speed : int = 150
+var bullet_speed : int = 125
 var angle : float = 0.0
 var is_attacking : bool = false
 
@@ -45,17 +45,17 @@ func start_cycle():
 func shoot():
 	var bullet_order = ["eb_diamond", "eb_square", "eb_round", "eb_round_big"]
 	for i in range(BULLET_COUNT):
-		var bullet_angle = angle + (TAU / BULLET_COUNT) * i
+		var bullet_angle = deg_to_rad(angle) + (TAU / BULLET_COUNT) * i
 		var direction = Vector2(cos(bullet_angle), sin(bullet_angle))
 		var bullet = BulletPool.get_bullet(bullet_order[bullet_index])
 		bullet.initialize(global_position, direction, bullet_speed, direction.angle())
 		
 	if bullet_index >= 3:
 		bullet_index = 0
-		bullet_speed = 150
+		#bullet_speed = 150
 	else:
 		bullet_index += 1
-		bullet_speed -= 20
+		#bullet_speed -= 20
 	angle += ANGLE_INCREMENT
 	burst_count += 1
 
