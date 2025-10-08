9a66436da8b22389b2616d513b23276465d22d64
VSocks <marvinpsilva2003@gmail.com>
2025-10-08 13:30:26 -0300

    organizados arquivos do jogo

---
+++ scripts/enemy_scripts/normal_attack/alternating_sides_attack.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+var fire_rate: float = 0.8
+var current_side: int = 0  # 0 = left, 1 = right
+
+@onready var timer = $Timer
+
+func _ready():
+	timer.timeout.connect(_on_timer_timeout)
+	timer.wait_time = fire_rate
+	timer.start()
+
+func _on_timer_timeout():
+	shoot_alternating()
+
+func shoot_alternating():
+	var angles = []
+	
+	if current_side == 0:
+		# Left side angles (pointing right/down-right)
+		angles = [deg_to_rad(0), deg_to_rad(30), deg_to_rad(60)]
+	else:
+		# Right side angles (pointing left/down-left)  
+		angles = [deg_to_rad(180), deg_to_rad(150), deg_to_rad(120)]
+	
+	for angle in angles:
+		var direction = Vector2(cos(angle), sin(angle))
+		var bullet = BulletPool.get_bullet("eb_diamond")
+		bullet.initialize(global_position, direction, 200, direction.angle())
+	
+	current_side = 1 - current_side  # Switch sides
